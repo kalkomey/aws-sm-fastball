eb1d824807e3018df5c7a0b1f274d34e5db1e1ed
Kevin Clark <kevin@texasviking.com>
2021-06-25 15:12:03 -0500

    get the secrets and write them to an app_config file

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
 # Container image that runs your code
 FROM ruby:2.6-alpine
 
+# Install necessary gems
+gem install aws-sdk-secretsmanager
+gem install fastball --source https://github.com/kalkomey/fastball
+
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
 COPY main.rb /main.rb
--- main.rb
+++ main.rb
@@ -1,11 +1,29 @@
-require 'date'
+require 'aws-sdk-secretsmanager'
+require 'json'
 
-p DateTime.now.to_s
+# appears to be running from repo folder.
 
-dirname = File.basename(Dir.getwd)
+# set variables
+aws_access_key_id 	  = ENV['aws_access_key_id']
+aws_secret_access_key = ENV['aws_secret_access_key']
+aws_region 			  = ENV['aws_region']
+secret_keys 		  = ENV['secret_keys']
 
-p dirname
+puts aws_access_key_id
+puts aws_secret_access_key
+puts aws_region
+puts secret_keys          
 
-folders = Dir.glob('*').select {|f| File.directory? f}
+credentials = Aws::Credentials.new(aws_access_key_id, aws_secret_access_key)
+sm = Aws::SecretsManager::Client.new(region: aws_region, credentials: credentials)
 
-p folders.to_a
+output = {}
+
+secret_keys.each do |secret_key|
+	secret = JSON.parse sm.get_secret_value(secret_id: secret_key).secret_string
+	output = output.merge(secret)
+end
+
+File.open('app_config.json', 'w') do |f|
+	f.puts output
+end
